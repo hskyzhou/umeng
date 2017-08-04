9c91eaa464d1a576041479993dbfbd3648a2e885
hsky <wen.zhou@bioon.com>
2017-08-04 16:06:45 +0800

    基本配置

---
+++ config/umeng.php
@@ -0,0 +1,21 @@
+<?php 
+
+return [
+	/**
+	 * 安卓 秘钥
+	 */
+	'android' => [
+		'app_key' => '',
+		'app_master_secret' => '',
+		'umeng_master_secret' => '',
+	],
+
+	/**
+	 * ios 秘钥
+	 */
+	'ios' => [
+		'app_key' => '',
+		'app_master_secret' => '',
+		'umeng_master_secret' => '',
+	]
+];
---
+++ src/ServiceProvider.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace HskyZhou\Umeng;
+
+/**
+ * ServiceProvider
+ */
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+	
+	/**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;	
+
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+    
+}
